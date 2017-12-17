d782505ad0272e61886e496a28f30b573b6a8f2f
a.romancha <a.romancha@hotmail.com>
2017-12-17 14:20:26 +0200

    Обновили toString

--- src/com/brainacad/oop/testshapes/Rectangle.java
+++ src/com/brainacad/oop/testshapes/Rectangle.java
@@ -3,19 +3,31 @@ package com.brainacad.oop.testshapes;
 /**
  * Created by User on 01.12.2017.
  */
-public class Rectangle extends Shape{
+public class Rectangle extends Shape {
     private double width;
     private double height;
 
-    public Rectangle(String colorShape, double width, double height){
+    public Rectangle(String colorShape, double width, double height) {
         super(colorShape);
         this.width = width;
         this.height = height;
     }
-    public double calcArea(){
+/*
+    public static Rectangle parseRectangle(String args) {
+        String[] params = args.split(":");
+        String[] sizes = params[2].split(",");
+        double a = Double.parseDouble(sizes[0]);
+        double b = Double.parseDouble(sizes[1]);
+        return new Rectangle(params[1], a, b);
+    }
+*/
+    public double calcArea() {
         return width * height;
     }
-    public String toString(){
-        return super.toString();
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + "width = " +
+                width + ", height = " + height;
     }
 }
--- src/com/brainacad/oop/testshapes/Triangle.java
+++ src/com/brainacad/oop/testshapes/Triangle.java
@@ -8,17 +8,30 @@ public class Triangle extends Shape {
     private double b;
     private double c;
 
-    public Triangle(String colorShape, double a, double b, double c){
+    public Triangle(String colorShape, double a, double b, double c) {
         super(colorShape);
         this.a = a;
         this.b = b;
         this.c = c;
     }
-    public double calcArea(){
-        double p = (a + b + c)/2;
+/*
+    public static Triangle parseTriangle(String args) {
+        String[] params = args.split(":");
+        String[] sizes = params[2].split(",");
+        double a = Double.parseDouble(sizes[0]);
+        double b = Double.parseDouble(sizes[1]);
+        double c = Double.parseDouble(sizes[2]);
+        return new Triangle(params[1], a, b, c);
+    }
+*/
+    public double calcArea() {
+        double p = (a + b + c) / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
-    public String toString(){
-        return super.toString();
+
+    @Override
+    public String toString() {
+        return super.toString() + ", a = " + a +
+                ", b = " + b + ", c = " + c;
     }
 }
